5af2a6456938b552a953a14578d6e8539171517f
yangzhihua <yangzhihuazzu@gmail.com>
2020-04-11 11:12:29 +0800

    2020-04-04
    1.测试往github中submit push;

--- src/news_spider/eastmoney_guba_spider.py
+++ src/news_spider/eastmoney_guba_spider.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 """
-eastmoney_guba_spider 东方财富股吧爬虫
+eastmoney_guba_spider 东方财富股吧
 """
 
 import time
@@ -14,9 +14,16 @@ def main():
     :return:
     """
     print()
-    driver = webdriver.chrome("D:\soft\chromedriver\chromedriver_win32\chromedriver.exe")
+    driver = webdriver.Chrome("D:\soft\chromedriver\chromedriver_win32\chromedriver.exe")
     driver.get("http://guba.eastmoney.com/")
     # 登录账号
+    # 沪市个股
+    url = "http://guba.eastmoney.com/remenba.aspx?type=1&tab=1"
+    # 深市个股
+    url = "http://guba.eastmoney.com/remenba.aspx?type=1&tab=2"
+
+    time.sleep(5)
+
 
 if __name__ == '__main__':
     main()
